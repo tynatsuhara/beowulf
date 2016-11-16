670fb6f2f87d4df4fb2c8d44c53384eac863cd3f
Tyler Bonnell <tbonnell@uw.edu>
2016-11-16 15:23:40 -0800

    gitignore

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
+	public GameObject weapon;
+	public GameObject shield;
+	public float health;
+	public bool isAlive {
+		get { return health > 0; }
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,13 +3,10 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float damage;
+	public float speed;
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void Attack() {
+		
 	}
 }

b64f0064b93e79730c93d4ceef17ea0d6575d6ac
Tyler Bonnell <tbonnell@uw.edu>
2016-11-26 13:00:37 -0800

    camera follow and zoom

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Character player;
+	public float followSpeed;
+	public float scrollMultiplier;
+	
+	void LateUpdate () {
+		if (player == null)
+			return;
+		
+		Vector3 toPos = player.stablePosition;
+		toPos.z = transform.position.z;
+		transform.position = Vector3.Lerp(transform.position, toPos, followSpeed);
+
+		Camera cam = GetComponent<Camera>();
+		cam.orthographicSize = Mathf.Clamp(cam.orthographicSize + Input.GetAxis("Mouse ScrollWheel") * -scrollMultiplier, .1f, 1.3f);
+	}
+}

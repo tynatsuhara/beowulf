685b0788a3981868b5baefb42bfc5338bd196378
Tyler Bonnell <tbonnell@uw.edu>
2016-12-05 18:48:31 -0800

    heorot start

---
+++ Assets/Scripts/SetChildSpriteMaterial.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class SetChildSpriteMaterial : MonoBehaviour {
+	
+	public Material material;
+
+	void Awake () {
+		GetComponentsInChildren<SpriteRenderer>().ToList().ForEach(x => x.material = material);
+	}
+}
